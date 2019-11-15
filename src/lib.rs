8b3ea26eaabde898a1a34aadf1e9396c51c7a986
james swash <james.swash@tocabot.io>
2019-11-15 19:56:17 +0000

    adding crc crate

--- src/lib.rs
+++ src/lib.rs
@@ -2,4 +2,8 @@ pub fn write_header() {
     // TODO Header Code here
 }
 
-pub fn write_body() {}
+pub fn write_chunks() {
+    //TODO Chunk code here
+}
+
+pub fn write_body<'a>(screen_data: &'a Vec<u8>) {}
